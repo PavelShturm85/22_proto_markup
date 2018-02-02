bfaacdef66bacdbb5b78b1e161f4b444cc9da741
PavelShturm85 <pavel.shturm@gmail.com>
2018-02-02 22:13:54 +0400

    сайт помещен в папку

--- site_reload.py
+++ site_reload.py
@@ -1,9 +1,7 @@
-from jinja2 import Template
-import json
 import os
 from livereload import Server
 
 
 if __name__ == '__main__':
     server = Server()
-    server.serve(root='')
+    server.serve(root='/site/')
